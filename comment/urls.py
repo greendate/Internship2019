1172e1a078dfefd92c7ccd64f2a838bcb6b89baa
greendate <n.novarlic@innopolis.ru>
2019-06-22 20:31:26 +0300

    Docker container added.

--- comment/urls.py
+++ comment/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.comments_list, name='comments_list'),
     path('new/', views.new_comment, name='new'),
     path('comment/<int:identify>/', views.comment, name='comment'),
+    path('author/', views.comments_list, name='empty_author'),
     path('author/<str:username>/', views.author, name='author'),
 ]
